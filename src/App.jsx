c07a648ec23143dea3689fc271f958ef71b858a6
happi89 <farhaanp99@gmail.com>
2022-06-26 23:48:04 -0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+  Advice #<!-- Advice ID goes here -->
+
+"<!-- Advice text goes here -->"
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  )
+}
+
+export default App
